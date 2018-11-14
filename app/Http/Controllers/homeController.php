7dbeea03fd56139e1268711372111c9deef04b3a
manhsanhcong <manhsanhcong.08@gmail.com>
2018-11-14 15:13:19 +0700

    add file

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use test\Http\Requests;
+
+class homeController extends Controller
+{
+    public function index()
+    {
+        return view('home');
+    }
+}
